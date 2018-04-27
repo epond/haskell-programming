a1489d3b7807a7b1ae6724244914076769744c6e
Edward Pond <edward.pond@springer.com>
2018-04-27 15:09:06 +0100

    Chapter 16 Exercises: Pair

--- src/Chapter16Functors.hs
+++ src/Chapter16Functors.hs
@@ -78,3 +78,8 @@ newtype Identity a = Identity a deriving (Eq, Show)
 
 instance Functor Identity where
     fmap f (Identity a) = Identity (f a)
+
+data Pair a = Pair a a deriving (Eq, Show)
+
+instance Functor Pair where
+    fmap f (Pair x y) = Pair (f x) (f y)
--- test/Chapter16FunctorsSpec.hs
+++ test/Chapter16FunctorsSpec.hs
@@ -16,8 +16,17 @@ spec = do
             property (functorIdentity :: Identity Int -> Bool)
         it "has a Functor instance for Identity that satisfies composition law" $ do
             property (\x -> (functorCompose (+1) (*2)) (x :: Identity Int))
+        it "has a Functor instance for Pair that satisfies identity law" $ do
+            property (functorIdentity :: Pair Int -> Bool)
+        it "has a Functor instance for Pair that satisfies composition law" $ do
+            property (\x -> (functorCompose (+1) (*2)) (x :: Pair Int))
 
 instance Arbitrary a => Arbitrary (Identity a) where
     arbitrary = do
         x <- arbitrary
         return (Identity x)
+
+instance Arbitrary a => Arbitrary (Pair a) where
+    arbitrary = do
+        x <- arbitrary
+        return (Pair x x)
