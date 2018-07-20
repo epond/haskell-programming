4354c648f92bd52fa2055a6885140169d3ab3298
epond <epond@users.noreply.github.com>
2018-07-20 16:53:54 +0100

    Chapter 17.8 ZipList Monoid

---
+++ projects/chapter17/src/ZipListMonoid.hs
@@ -0,0 +1,21 @@
+module ZipListMonoid where
+
+import Control.Applicative
+import Data.Monoid
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+instance Monoid a => Monoid (ZipList a) where
+    mempty = pure mempty
+    mappend = liftA2 mappend
+
+-- No need for these Arbitrary instances since they are defined in Test.QuickCheck
+-- instance Arbitrary a => Arbitrary (ZipList a) where
+--     arbitrary = ZipList <$> arbitrary
+--
+-- instance Arbitrary a => Arbitrary (Sum a) where
+--     arbitrary = Sum <$> arbitrary
+
+instance Eq a => EqProp (ZipList a) where
+    (=-=) = eq
