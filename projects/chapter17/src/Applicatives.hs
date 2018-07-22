6b10e1a08f4086b17586ffdaa2acbb35f5e83062
epond <epond@users.noreply.github.com>
2018-07-22 16:41:27 +0100

    Chapter 17.8 List Applicative

--- projects/chapter17/src/Applicatives.hs
+++ projects/chapter17/src/Applicatives.hs
@@ -1,6 +1,7 @@
 module Applicatives where
 
 import Control.Applicative
+import Data.Monoid
 import Data.List (elemIndex)
 
 -- 17.5 Exercises: Lookups
@@ -80,3 +81,45 @@ instance Monoid a => Applicative (Constant a) where
 
 -- Instead, bottom can be used with explicit type to achieve the same effect:
 --   quickBatch $ applicative (undefined :: [(String, String, Int)])
+
+-- 17.8 List Applicative Exercise
+
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+instance Functor List where
+    -- fmap :: (a -> b) -> f a -> f b
+    fmap _ Nil = Nil
+    fmap f (Cons x rest) = Cons (f x) $ fmap f rest
+
+append :: List a -> List a -> List a
+append Nil ys = ys
+append (Cons x xs) ys = Cons x $ xs `append` ys
+
+fold :: (a -> b -> b) -> b -> List a -> b
+fold _ b Nil        = b
+fold f b (Cons h t) = f h (fold f b t)
+
+concat' :: List (List a) -> List a
+concat' = fold append Nil
+
+-- write this one in terms
+-- of concat' and fmap
+flatMap :: (a -> List b) -> List a -> List b
+flatMap f as = concat' $ fmap f as
+
+instance Monoid (List a) where
+    mempty = Nil
+    x `mappend` y = x `append` y
+
+instance Applicative List where
+    -- pure :: a -> f a
+    pure x = Cons x (Nil)
+    -- (<*>) :: f (a -> b) -> f a -> f b
+    Nil <*> _ = Nil
+    _ <*> Nil = Nil
+    Cons f fs <*> xs = (f <$> xs) <> (fs <*> xs)
+
+runListApp :: List Int
+runListApp = f <*> v
+    where f = Cons (+1) (Cons (*2) Nil)
+          v = Cons 1 (Cons 2 Nil)
