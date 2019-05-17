10f8b2a1a65e56221cba7b101219725bcb0a5ca7
Ed <epond@users.noreply.github.com>
2019-05-17 14:28:53 +0100

    Chapter 21.12 Exercise: Identity

--- projects/chapter21/src/Traversable.hs
+++ projects/chapter21/src/Traversable.hs
@@ -1,3 +1,16 @@
 module Traversable where
 
 -- 21.12 Chapter Exercises
+
+-- Identity
+newtype Identity a = Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+
+instance Foldable Identity where
+  foldMap f (Identity x) = f x
+
+instance Traversable Identity where
+  traverse f (Identity x) = Identity <$> f x
--- projects/chapter21/test/TraversableSpec.hs
+++ projects/chapter21/test/TraversableSpec.hs
@@ -2,9 +2,24 @@ module TraversableSpec where
 
 import Traversable
 import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
 
 spec :: Spec
 spec = do
   describe "21.12 Chapter Exercises" $ do
-    it "sanity" $ do
-      True `shouldBe` True
+    it "Identity instance of Traversable" $ do
+      hspec $ testBatch (traversable (undefined :: Identity (Int, Int, [Int])))
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+    x <- arbitrary
+    return (Identity x)
+
+instance Eq a => EqProp (Identity a) where (=-=) = eq
+
+-- | Allows to insert a 'TestBatch' into a Spec. (code taken from hspec-checkers library)
+testBatch :: TestBatch -> Spec
+testBatch (batchName, tests) = describe ("laws for: " ++ batchName) $
+    foldr (>>) (return ()) (map (uncurry it) tests)
